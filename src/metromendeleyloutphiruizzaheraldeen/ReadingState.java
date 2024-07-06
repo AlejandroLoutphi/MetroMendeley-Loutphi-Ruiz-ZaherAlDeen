3da74e7213e51b6a62ed8ff26841a140d0db7838
AlejandroLoutphi <alejandroloutphi@gmail.com>
2024-07-06 00:05:31 -0400

    Added JavaDoc and cleaned up Functions. Split ReadingState.

---
+++ src/metromendeleyloutphiruizzaheraldeen/ReadingState.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package metromendeleyloutphiruizzaheraldeen;
+
+/**
+ * Enum for holding the value of what the text parser functions are currently
+ * reading.
+ *
+ * @author Alejandro Loutphi
+ */
+public enum ReadingState {
+    TITLE,
+    AUTHORS,
+    TEXT,
+    KEYWORDS,
+}
+
